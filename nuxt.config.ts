48d876001dca15b7aa12094efaf62c498aa7bffe
SunYenn <syedk@naver.com>
2024-01-31 11:03:52 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'nuxt3-starter-package',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@element-plus/nuxt'
+  ],
+  build: {
+  }
+})
